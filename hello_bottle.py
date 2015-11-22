ba4eb39f6382a610e5c6bbd127ade183485dcf0e
Pedro Rodrigues <medecau@gmail.com>
2015-11-22 13:12:42 +0000

    making the example as similar as possible

--- hello_bottle.py
+++ hello_bottle.py
@@ -1,10 +1,12 @@
-from bottle import Bottle, run
+from bottle import Bottle
+
 
 app = Bottle()
 
+
 @app.route('/')
-def hello():
+def hello_world():
     return "Hello World!"
 
 if __name__ == '__main__':
-    run(app, host='localhost', port=8080)
+    app.run()

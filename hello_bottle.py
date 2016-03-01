cfc6d7c1ddbf0407fb446ca081826721162c534f
Pedro Rodrigues <medecau@gmail.com>
2016-03-01 23:17:34 +0000

    added lots of boilerplate
    new files for:
    bottle.py
    bootstrap
    jQuery
    
    removed flask version
    
    bottle version working with jinja2 views
    and an example ajax request using jQuery

--- hello_bottle.py
+++ hello_bottle.py
@@ -1,12 +1,23 @@
-from bottle import Bottle
+from bottle import Bottle, jinja2_view as view, static_file, response
 
 
-app = Bottle()
+app = application = Bottle()
 
+@app.route('/s/<filepath:path>')
+def serve_static(filepath):
+    return static_file(filepath, root='static/')
 
 @app.route('/')
+@view('home.html')
 def hello_world():
-    return "Hello World!"
+    #return "Hello World! // %s - %s - %s" % (counter, os.getpid(), time.time())
+    return dict(msg='hello')
+
+@app.route('/api/hello')
+def hello():
+    return '<b>hello</b>'
+
 
 if __name__ == '__main__':
-    app.run()
+    #app.run(quiet=True)
+    app.run(debug=True, reloader=True)
